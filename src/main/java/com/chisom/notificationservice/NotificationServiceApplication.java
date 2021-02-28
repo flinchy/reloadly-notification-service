970427f143706ff1263d2550217f838617b43c94
chisom <iwowochisom@gmail.com>
2021-02-28 19:56:25 +0100

    fix bug

--- src/main/java/com/chisom/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/chisom/notificationservice/NotificationServiceApplication.java
@@ -2,10 +2,7 @@ package com.chisom.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling
 @SpringBootApplication
@@ -13,11 +10,5 @@ public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
