e4a49d3747c93d5abcc40545231ab24831bdf19e
chisom <iwowochisom@gmail.com>
2021-02-28 17:33:26 +0100

    add notification constants

---
+++ src/main/java/com/chisom/notificationservice/constants/NotificationConstants.java
@@ -0,0 +1,9 @@
+package com.chisom.notificationservice.constants;
+
+public final class NotificationConstants {
+
+    private NotificationConstants() {
+    }
+
+    public static final String FROM = "chisom@noreply.com";
+}
